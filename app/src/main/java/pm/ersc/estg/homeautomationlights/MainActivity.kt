2027f0427a0cd2e282bc011045720c1844009e0e
johnsteps <joaomiguelpassos@outlook.pt>
2021-01-16 01:22:18 +0000

    DONE:Connecting to ESP TODO: Write to characteristic and layout

--- app/src/main/java/pm/ersc/estg/homeautomationlights/MainActivity.kt
+++ app/src/main/java/pm/ersc/estg/homeautomationlights/MainActivity.kt
@@ -1,6 +1,7 @@
 package pm.ersc.estg.homeautomationlights
 
 import android.Manifest
+import android.app.Activity
 import android.app.admin.ConnectEvent
 import android.bluetooth.*
 import android.bluetooth.le.ScanCallback
@@ -9,6 +10,8 @@ import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.ParcelUuid
 import android.util.Log
@@ -20,30 +23,63 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
 import org.jetbrains.anko.Android
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.toast
+import pm.ersc.estg.homeautomationlights.ble.ConnectionManager
+import timber.log.Timber
 import java.util.*
 
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
+private const val TAG = "****"
+private const val ServUUID = "ab0828b1-198e-4351-b779-901fa0e0371e"
+private const val CHARUUID = "4ac8a682-9736-4e5d-932b-e9b31405049c"
+private const val RED = 0x11
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private val TAG = "****"
-        const val REQUEST_ENABLE_BT = 1
-        const val UUIDservice = "ab0828b1-198e-4351-b779-901fa0e0371e"
-    }
+    private var blueDevice: BluetoothDevice? = null
+    private lateinit var ledCharacteristic: BluetoothGattCharacteristic
 
     private val bluetoothAdapter: BluetoothAdapter by lazy {
-        (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
     }
+
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .build()
+
+    private var isScanning = false
+    /*set(value) {
+        field = value
+        runOnUiThread { connectDevice.text = if (value) "Stop Scan" else "Start Scan" }
+    }*/
+
+    private val scanResults = mutableListOf<ScanResult>()
+
+    private val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    /*******************************************
+     * Activity function overrides
+     *******************************************/
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         //Variables (Buttons and Textviews)////////////////////////////////////
         val connectedtextview = findViewById<TextView>(R.id.connectedDevice)
-        val ConnectDevice = findViewById<Button>(R.id.selectdevice)
+        val connectDevice = findViewById<Button>(R.id.selectdevice)
         val DisconnectDevice = findViewById<Button>(R.id.disconnect_from_device)
         val RedSelection = findViewById<Button>(R.id.REDButton)
         val GreenSelection = findViewById<Button>(R.id.GREENButton)
@@ -61,102 +97,119 @@ class MainActivity : AppCompatActivity() {
         }
 
         //Scanner Button
-        ConnectDevice?.setOnClickListener() { }
+        connectDevice?.setOnClickListener() { if (isScanning) stopBleScan() else startBleScan() }
 
         //Color Buttons and actions
-        RedSelection?.setOnClickListener() { toast("Red Color") }
+        RedSelection?.setOnClickListener() {
+            //ConnectionManager.writeCharacteristic(blueDevice, ,0x11)
+
+        }
+
         GreenSelection?.setOnClickListener() { toast("Green Color") }
         BlueSelection?.setOnClickListener() { toast("Blue Color") }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
+                    requestLocationPermission()
+                } else {
+                    startBleScan()
+                }
+            }
+        }
+    }
 
-    override fun onResume() {
-        super.onResume()
-        if (bluetoothAdapter.isEnabled) {
-            toast("Bluetooth Already Enabled!")
-            startBleScan(this, UUID.fromString(UUIDservice))
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+
+    private fun startBleScan() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
+            requestLocationPermission()
         } else {
-            Log.v(TAG, "BT is disabled!")
-            val btIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(btIntent, REQUEST_ENABLE_BT)
+            scanResults.clear()
+            //scanResultAdapter.notifyDataSetChanged()
+            bleScanner.startScan(null, scanSettings, scanCallback)
+            isScanning = true
         }
     }
 
-
-    private fun startBleScan(context: Context, serviceUUID: UUID) {
-        Log.v(TAG, "Start Scan pt1")
-
-        val uuid = ParcelUuid(serviceUUID)
-        val filter = ScanFilter.Builder().setServiceUuid(uuid).build()
-        val filters = listOf(filter)
-
-        val settings = ScanSettings.Builder()
-            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build()
-
-        Log.v(TAG, "Start Scan pt2")
-
-        context.runWithPermissions(Manifest.permission.ACCESS_FINE_LOCATION){
-            bluetoothAdapter.bluetoothLeScanner.startScan(filters, settings, bleScanCallback)
-        }
-
+    private fun stopBleScan() {
+        bleScanner.stopScan(scanCallback)
+        isScanning = false
     }
 
-    private val bleScanCallback = object : ScanCallback() {
-        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
-        results?.forEach{ result -> deviceFound(result.device)}
+    private fun requestLocationPermission() {
+        if (isLocationPermissionGranted) {
+            return
         }
-
-        override fun onScanResult(callbackType: Int, result: ScanResult?) {
-            //result?.let { deviceFound(result.device) }
-            Log.v(TAG,"Founded Device : " + "${result?.device}")
+        runOnUiThread {
+            alert {
+                title = "Location permission required"
+                message = "Starting from Android M (6.0), the system requires apps to be granted " +
+                        "location access in order to scan for BLE devices."
+                isCancelable = false
+                positiveButton(android.R.string.ok) {
+                    requestPermission(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        LOCATION_PERMISSION_REQUEST_CODE
+                    )
+                }
+            }.show()
         }
-
-        override fun onScanFailed(errorCode: Int) {
-           Log.v(TAG, "Scan Failed to execute")
-        }
-
     }
 
-    private fun deviceFound(device: BluetoothDevice){
-        device.connectGatt(this, true, gattCallback)
-    }
+    /*******************************************
+     * Callback bodies
+     *******************************************/
 
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val indexQuery = scanResults.indexOfFirst { it.device.address == result.device.address }
+            if (indexQuery != -1) { // A scan result already exists with the same address
+                scanResults[indexQuery] = result
+                //scanResultAdapter.notifyItemChanged(indexQuery)
+            } else {
+                with(result.device) {
+                    Timber.i("Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
+                    if(name == "ESP32-BLE"){
+                        blueDevice = this
+                        stopBleScan()
+                        ConnectionManager.connect(this, this@MainActivity)
+                    }
+                }
+                scanResults.add(result)
+                //scanResultAdapter.notifyItemInserted(scanResults.size - 1)
 
-    private val gattCallback = object : BluetoothGattCallback(){
-        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
-            if (newState == BluetoothGatt.STATE_CONNECTED){
-                gatt?.requestMtu(256)
-                gatt?.discoverServices()
             }
         }
 
-        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-
+        override fun onScanFailed(errorCode: Int) {
+            Timber.e("onScanFailed: code $errorCode")
         }
+    }
 
-        override fun onCharacteristicRead(
-            gatt: BluetoothGatt?,
-            characteristic: BluetoothGattCharacteristic?,
-            status: Int
-        ) {
-            super.onCharacteristicRead(gatt, characteristic, status)
-        }
+    /*******************************************
+     * Extension functions
+     *******************************************/
 
-        override fun onCharacteristicWrite(
-            gatt: BluetoothGatt?,
-            characteristic: BluetoothGattCharacteristic?,
-            status: Int
-        ) {
-            super.onCharacteristicWrite(gatt, characteristic, status)
-        }
+    private fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
 
-        override fun onCharacteristicChanged(
-            gatt: BluetoothGatt?,
-            characteristic: BluetoothGattCharacteristic?
-        ) {
-            super.onCharacteristicChanged(gatt, characteristic)
-        }
-
+    private fun Activity.requestPermission(permission: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
     }
 }
 
